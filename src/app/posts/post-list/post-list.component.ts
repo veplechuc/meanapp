e32ba1852ed05ec6bbcb265d8a80e802ff96881b
valentin plechuc <Valentin.Plechuc.Contractor@bydeluxe.com>
2019-03-19 17:22:20 -0300

    Refactor list html showing info Refactor component list to use observables in onInit and onDestroy methods

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Post } from '../post.model';
+import { PostService } from '../posts.service';
+import {Subscription} from 'rxjs'
+
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  // posts = [{ title: "fisrt", content: "algo" },
+  //         { title: "second", content: "algo2" }]
+  componentPosts: Post[] = [];
+  private postsSub: Subscription;
+  
+  constructor(public postService: PostService) { }
 
   ngOnInit() {
+    this.componentPosts = this.postService.getPosts();
+    // make an observable to be able to get the data
+    // using a subscription in order to unload any  subscription for this component 
+    // when no longer in defined in the doom when destroy
+    this.postsSub = this.postService.getPostsUpdatedListener()
+    .subscribe((posts: Post[])=>{
+      this.componentPosts = posts
+
+    })
   }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    // prevents memory leaks
+    this.postsSub.unsubscribe();
+  }
+
 
 }
